f52944da1db68a9a1c5a1eca1f2eb6b76bfc1110
tahazain786 <139646246+tahazain786@users.noreply.github.com>
2026-04-02 13:53:19 +0530

    Add Java program to demonstrate comments and function
    Implemented a Java program that demonstrates function calling, single line, multi-line, and documentation comments.

---
+++ Arrays/callingFunction.java
@@ -0,0 +1,26 @@
+
+public class callingFunction {
+	 /** 1.Write a function to print your name and call the function from main method.
+	  *  2.Write a program for a Single line comment, multi-line and documentation comments.
+	  *  3.How to create a class, object, method and its signature.**/
+	
+	static void myName() {
+		System.out.println("My name is Muskaan Fatima");
+	}
+	
+	public static void main(String[] args) {
+		myName();
+		System.out.println("Single line comments are given by //");
+		//System.out.println("Hi");
+		System.out.println("Multi line comments are given by /* */");
+		/*System.out.println("Hi");
+		 * hey
+		 */
+		System.out.println("Documentation comments are given by /** */");
+		/**
+		 * hif\
+		 * kjdkfj
+		 * kdsjfj
+		 */
+	}
+}
